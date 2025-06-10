3ea0e2c4d21c7757d7968c15be0f8f310b00367b
Felipe Bellini <felipelemosbellini@gmail.com>
2025-06-10 00:15:04 -0300

    fixed balance and address

--- lib/ui/features/home/wallet/wallet_controller.dart
+++ lib/ui/features/home/wallet/wallet_controller.dart
@@ -1,40 +1,35 @@
 import 'package:get_it/get_it.dart';
 import 'package:tcc/core/helpers/base_controller.dart';
+import 'package:tcc/core/models/user_model.dart';
 import 'package:tcc/core/repositories/firestore/firestore_repository_interface.dart';
 import 'package:tcc/ui/features/home/home_controller.dart';
+import 'package:tcc/ui/widgets/dialogs/alert_helper.dart';
 
 class WalletController extends BaseController {
   final HomeController _homeController = GetIt.I.get<HomeController>();
-  final FirestoreRepositoryInterface _firestoreRepository = GetIt.I.get<FirestoreRepositoryInterface>();
+  final FirestoreRepositoryInterface _firestoreRepository =
+      GetIt.I.get<FirestoreRepositoryInterface>();
 
-  double? get balance => _homeController.balance;
+  UserModel? userModel;
 
-  // Método que retorna só o endereço do usuário (ou null, se não tiver)
-  Future<String?> getUserAddress() async {
-    final result = await _firestoreRepository.getUser();
-    return result.fold(
-          (exception) => null,
-          (user) => user.address,
-    );
-  }
-
-  Future<double> loadAssets() async {
+  Future<void> loadUser() async {
     _homeController.setLoading(true);
-
     final result = await _firestoreRepository.getUser();
-    if (result.isLeft()) {
-      _homeController.setLoading(false);
-      return 0.0;
-    }
-
-    final user = result.getRight().toNullable();
-    if (user == null) {
-      _homeController.setLoading(false);
-      return 0.0;
-    }
-
-    await Future.delayed(const Duration(seconds: 1));
+    result.fold(
+      (error) {
+        setMessage(
+          AlertData(
+            message: "Erro ao criar o testamento",
+            errorType: ErrorType.error,
+          ),
+        );
+        return;
+      },
+      (UserModel user) {
+        userModel = user;
+        notifyListeners();
+      },
+    );
     _homeController.setLoading(false);
-    return user.balance;
   }
 }
