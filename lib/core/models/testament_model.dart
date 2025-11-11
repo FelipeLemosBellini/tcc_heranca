24fdd8c8365c4aedbcf0cb7c3e10a73f6465bcf7
felipe.bellini <felipelemosbellini@gmail.com>
2025-11-11 19:34:31 -0300

    removed firebase

--- lib/core/models/testament_model.dart
+++ lib/core/models/testament_model.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class TestamentModel {
   String testamentAddress;
   DateTime dateCreated;
@@ -28,29 +26,12 @@ class TestamentModel {
     };
   }
 
-  factory TestamentModel.fromDocument(
-    QueryDocumentSnapshot<Map<String, dynamic>> doc,
-  ) {
-    return TestamentModel(
-      testamentAddress: doc["testamentAddress"],
-      value: doc["value"],
-      dateCreated: doc["dateCreated"],
-    );
-  }
-
   factory TestamentModel.fromMap(Map<String, dynamic> map) {
     return TestamentModel(
       testamentAddress: map['testament'],
       dateCreated: DateTime.parse(map['dateCreated']),
       value: (map['value'] as num).toDouble(),
     );
-  }
-
-  factory TestamentModel.fromSnapshot(
-    DocumentSnapshot<Map<String, dynamic>> doc,
-  ) {
-    final data = doc.data()!;
-    return TestamentModel.fromMap({...data, 'testament': doc.id});
   }
 
   TestamentModel copyWith({
